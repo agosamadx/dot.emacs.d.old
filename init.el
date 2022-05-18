0edfbfcdf94f0574f2c0190098194f146a59e9e2
ago <ago@autoexec.jp>
2022-05-18 18:17:12 +0900

    refactor

--- init.el
+++ init.el
@@ -149,9 +149,7 @@
   (global-set-key (kbd "C-M-/") 'undo-tree-redo))
 (setq undo-tree-auto-save-history nil)
 
-(when (require 'csharp-mode nil t)
-  (defun my-csharp-mode-setup ()
-    )
+(when (require 'omnisharp-mode nil t)
   (add-hook 'csharp-mode-hook
             (lambda ()
               (omnisharp-mode)
